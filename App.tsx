a278a9fb795f619bd1d11167bf13fc13cd3e7d6f
yJoaoVictor10 <jvna07@gmail.com>
2026-03-05 21:08:40 -0300

    projeto finalizado

--- App.tsx
+++ App.tsx
@@ -1,26 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+const DAILY_GOAL:number = 8; // Total diário de copos
 
 export default function App() {
+
+  const[cups, setCups] = useState<number>(0);
+
+  const percentage:number = (cups/DAILY_GOAL) * 100;
+
+  const removeCup = ()=>{
+    if(cups > 0){
+      setCups(cups - 1);
+    }
+  }
+
   return (
     <View style={styles.container}>
       {/* Cabeçalho */}
       <View style={styles.header}>
-        <Text style={styles.appTitle}>Beba Água2 💧</Text>
+        <Text style={styles.appTitle}>Beba Água 💧</Text>
         <Text style={styles.appSubtitle}>HIDRATAÇÃO DIÁRIA</Text>
       </View>
 
       {/* Seção Principal / Indicador Visual */}
       <View style={styles.content}>
+
+        <View style={styles.outerCircleWrapper}></View>
+
+       
         {/* Círculo */}
         <View style={styles.outerCircle}>
           {/* Nível da água */}
-          <View style={styles.waterLevel} />
+          <View style={[styles.waterLevel, {height: `${percentage}%`}]} />
+
+          {/* Quantidade de copos tomados */}
+          <Text style={styles.numberText}>{cups}</Text>
+          <Text style={styles.labelText}>COPOS</Text>
         </View>
+
+        {/* Feedback */}
+        <View style={styles.feedbackContainer}>
+            <Text style={[styles.statusText, cups >= DAILY_GOAL && {color: '#059669'} ]}>{cups >= DAILY_GOAL ? "Parabéns! Meta batida!" : `Faltam ${DAILY_GOAL - cups} copos para a meta.`}</Text>
+        {/* Barra de Progresso */}
+        <View style={styles.progressBarBackground}>
+          <View style={[styles.progressBar, {width: `${percentage}%`}]}>
+          </View>
+        </View>
+        </View>
+
+
       </View>
 
-      {/* Botões */}
-      <Text>Primeiro app da 2TDSPX!!! 2026</Text>
+
+
+      {/* Botões / Rodapé */}
+      <View style={styles.footer}>
+        {/* Botão Principal */}
+        <TouchableOpacity style={styles.mainButton} onPress={()=> setCups(cups + 1)}>
+          <Text style={styles.mainButtonText}>BEBER 1 COPO (200 ML)</Text>
+        </TouchableOpacity>
+        {/* Botões de ajuste */}
+        <View style={styles.adjustmentArea}>
+          <TouchableOpacity style={styles.adjustButton} onPress={removeCup} >
+              <Text style={styles.adjustButtonText}>Remover</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.adjustButton} onPress={()=> setCups(0)}>
+            <Text style={styles.adjustButtonText}>Reeiniciar</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -54,6 +106,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '100%',
   },
+
+  outerCircleWrapper: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 10,
+    backgroundColor: "#FFF",
+    borderRadius: 110,
+  },
+
   outerCircle: {
     width: 220,
     height: 220,
@@ -63,19 +126,96 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     overflow: 'hidden',
-    elevation: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 5 },
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
+    backgroundColor: "#FFF"
+
   },
   waterLevel: {
     position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
-    height: 60,
     backgroundColor: '#0EA5E9',
     opacity: 0.2,
+  },
+
+  numberText: {
+    fontSize: 72,
+    fontWeight: '900',
+    color: '#0284C7',
+  },
+
+  labelText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#7DD3FC',
+  },
+
+  progressBarBackground: {
+    width: '100%',
+    height: 12,
+    backgroundColor: '#E0F2FE',
+    borderRadius: 6,
+    marginTop: 15,
+  },
+
+  progressBar:{
+    height: '100%',
+    width: '70%',
+    backgroundColor: '#0284C7',
+    borderRadius: 6,
+  },
+
+  feedbackContainer: {
+    marginTop: 40,
+    alignItems: 'center',
+    width: '80%',
+  },
+
+  statusText: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#475569',
+    textAlign: 'center',
+  },
+
+  footer: {
+    width: '100%',
+    paddingHorizontal: 30,
+    marginBottom: 20,
+  },
+
+  mainButton: {
+    backgroundColor: '#0284C7',
+    paddingVertical: 22,
+    borderRadius: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 5, // elevation para sombra (Android)
+  },
+
+  mainButtonText: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '900',
+  },
+
+  adjustmentArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
+
+  adjustButton: {
+    padding: 10,
+  },
+
+  adjustButtonText: {
+    color: '#94A3BB',
+    fontSize: 14,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
   }
 });
